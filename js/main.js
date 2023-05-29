7d8022d4505fa079e6357225ba27886be293e07a
Martinraju11 <martinraju53@gmail.com>
2023-05-29 19:33:22 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var n=parseInt(prompt("Enter the limit of reverse sequence"));
+document.write("Reverse of sequence of numbers");
+	document.write("<br>");
+for(i=n;i>=0;i--)
+{
+	document.write(i);
+	document.write("<br>");
+}
